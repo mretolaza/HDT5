52ac5d0066d3f3cf09b7932ad25d66e977e8fb65
ret16339 <ret16339@uvg.edu.gt>
2017-02-22 08:39:41 -0600

    Librerias

--- ProcesamientoRam.py
+++ ProcesamientoRam.py
@@ -3,3 +3,14 @@
 #Carne 16339, Seccion 20 
 #Algortimos y Estructura de Datos 
 
+
+#Se importan las librerias random y simpy para realizar el simualdor 
+import simpy  
+import random
+
+def operacion(nombre,env,memoria):
+    global tiempoMemoria
+    while True:
+        new_duration = random.randint(1,10)
+        print(nombre, 'pidio', new_duration, 'de memoria en el tiempo:',env.now)
+        tiempoPedir = env.now
