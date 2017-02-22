637ed8f5f6dd9ce3ab60357cc77782d4da816a8c
ret16339 <ret16339@uvg.edu.gt>
2017-02-22 07:48:02 -0600

    Comentarios

--- ProcesamientoRam.py
+++ ProcesamientoRam.py
@@ -2,6 +2,9 @@
 #Maria Mercedes Retolaza Reyna 
 #Carne 16339, Seccion 20 
 #Algortimos y Estructura de Datos 
+#El programa se encarga de simular como funciona una memoria RAM a un entorno, se crean diferentes procesos 
+#En un espacio de memoria dado, el objetivo es ver cuanto a almacenado a la cola y asi identificar que tipo es 
+# mas rapido 
 
 
 #Se importan las librerias random y simpy para realizar el simualdor 
@@ -14,3 +17,11 @@ def operacion(nombre,env,memoria):
         new_duration = random.randint(1,10)
         print(nombre, 'pidio', new_duration, 'de memoria en el tiempo:',env.now)
         tiempoPedir = env.now
+
+         with memoria.request() as turno:
+            yield turno
+            yield env.timeout(new_duration)
+            print(nombre,'libero memoria a las',env.now)
+            tiempoTotal = env.now - tiempoPedir
+            print('%s se tardo %d' % (nombre,tiempoTotal ))
+            tiempoMemoria = tiempoMemoria+tiempoTotal
